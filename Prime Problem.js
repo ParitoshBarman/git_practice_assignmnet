90e762db0c3745348ae5e63459e1d2c7bf0c60b2
Paritosh Barman <barmanpari163@gmail.com>
2023-10-24 19:25:27 +0530

    First commit

---
+++ Prime Problem.js
@@ -0,0 +1,15 @@
+// This is mistake file
+
+let num = 20;
+
+let sum = 0;
+for(let i=0; i<num; i++){
+    if(num%i==0){
+        sum++
+    }
+}
+if(sum==2){
+    console.log("Not Prime");
+}else{
+    console.log("Not Prime");
+}
